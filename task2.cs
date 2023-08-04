8faa751b05cdfbebf29144cae9ee4ca0dd0424c7
timbokido <39024286+timbokido@users.noreply.github.com>
2023-08-04 12:43:26 +0200

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,10 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int [] array = new int[10];
+int count = 0;
+for (int i = 0; i < 10; i++){
+    array[i] = new Random().Next(0, 100);
+    if (array[i]%2==1) count+=array[i];
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine($"сумма нечетных элементов равняется {count}");
---
+++ task3.cs
@@ -0,0 +1,13 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
+
+double [] array = new double[10];
+double max = -1;
+double min = 11;
+for (int i = 0; i < 10; i++){
+    array[i] = new Random().NextDouble() * 10;
+    if (array[i]>max) max=array[i];
+    if (array[i]<min) min=array[i];
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine("");
+Console.WriteLine($"{max} - {min} = {max-min}");
